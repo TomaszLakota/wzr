ed284a608c362f794df3231193f6684885101f5b
Tomasz Łakota <48764424+TomaszLakota@users.noreply.github.com>
2025-04-22 23:24:10 +0200

    fix capitalized folder name part 1

--- src/pages/lesson/LessonPage.tsx
+++ src/pages/lesson/LessonPage.tsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import './LessonPage.scss';
-import Lesson from '../../components/lesson/Lesson';
+import Lesson from '../../components/lesson-/Lesson';
 
 const LessonPage = () => {
   const { id } = useParams();

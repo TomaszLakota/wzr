ab982e8807da2797ad6cb13ae99fc1eb42cf4a23
Tomasz Łakota <48764424+TomaszLakota@users.noreply.github.com>
2025-05-13 23:09:43 +0200

    fix: links should scroll to top

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
+import ScrollToTop from '../../utils/ScrollToTop';
 import './Layout.scss';
 
 const Layout: React.FC = () => {
   return (
     <div className="layout-container">
+      <ScrollToTop />
       <Header />
       <main>
         <Outlet />
